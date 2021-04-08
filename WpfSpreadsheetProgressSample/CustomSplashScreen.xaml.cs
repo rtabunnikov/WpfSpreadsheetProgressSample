4b3d9e08db606b2b5f405014828b31eb26bff08c
Roman Tabunnikov <roman.tabunnikov@devexpress.com>
2021-04-08 15:31:42 +0300

    Custom splash screen + cancellation

---
+++ WpfSpreadsheetProgressSample/CustomSplashScreen.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Input;
+using DevExpress.Xpf.Core;
+
+namespace WpfSpreadsheetProgressSample {
+    public partial class CustomSplashScreen : SplashScreenWindow {
+        readonly CancellationTokenSource cancellationTokenSource;
+
+        public CustomSplashScreen(CancellationTokenSource cancellationTokenSource) {
+            this.cancellationTokenSource = cancellationTokenSource;
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, MouseButtonEventArgs e) {
+            cancellationTokenSource?.Cancel();
+        }
+    }
+}
--- WpfSpreadsheetProgressSample/MainWindow.xaml.cs
+++ WpfSpreadsheetProgressSample/MainWindow.xaml.cs
@@ -1,26 +1,15 @@
-﻿using DevExpress.Mvvm;
+﻿using System;
+using System.Threading;
+using DevExpress.Mvvm;
+using DevExpress.Office.Services;
+using DevExpress.Office.Services.Implementation;
 using DevExpress.Services;
 using DevExpress.Xpf.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfSpreadsheetProgressSample {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : ThemedWindow, IProgressIndicationService {
+        CancellationTokenSource cancellationTokenSource;
+        ICancellationTokenProvider savedCancellationTokenProvider;
         SplashScreenManager splashScreenManager;
 
         public MainWindow() {
@@ -29,8 +18,10 @@ namespace WpfSpreadsheetProgressSample {
         }
 
         void IProgressIndicationService.Begin(string displayName, int minProgress, int maxProgress, int currentProgress) {
-            splashScreenManager = SplashScreenManager.CreateThemed(new DXSplashScreenViewModel {
-                IsIndeterminate = false,
+            cancellationTokenSource = new CancellationTokenSource();
+            savedCancellationTokenProvider = spreadsheetControl1.ReplaceService<ICancellationTokenProvider>(new CancellationTokenProvider(cancellationTokenSource.Token));
+            splashScreenManager = SplashScreenManager.Create(() => new CustomSplashScreen(cancellationTokenSource), new DXSplashScreenViewModel {
+                Title = displayName,
                 Progress = currentProgress
             });
             splashScreenManager.Show();
@@ -39,10 +30,19 @@ namespace WpfSpreadsheetProgressSample {
         void IProgressIndicationService.End() {
             splashScreenManager?.Close();
             splashScreenManager = null;
+            spreadsheetControl1.ReplaceService(savedCancellationTokenProvider);
+            spreadsheetControl1.UpdateCommandUI();
+            cancellationTokenSource?.Dispose();
+            cancellationTokenSource = null;
         }
 
         void IProgressIndicationService.SetProgress(int currentProgress) {
             splashScreenManager.ViewModel.Progress = currentProgress;
+        }
+
+        void spreadsheetControl1_UnhandledException(object sender, DevExpress.XtraSpreadsheet.SpreadsheetUnhandledExceptionEventArgs e) {
+            if (e.Exception is OperationCanceledException)
+                e.Handled = true;
         }
     }
 }
